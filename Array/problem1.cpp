5f053a80924c4e54d98be74a59c13678fac67074
SadmanShahriar75 <sadmanshahriar7576@gmail.com>
2025-02-17 07:02:38 +0600

    hi

--- Array/problem1.cpp
+++ Array/problem1.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-void generatePascalsTriangle(int numRows) {
+void pascalsTriangle(int numRows) {
     int pascal[numRows][numRows];
 
     for (int i = 0; i < numRows; i++) {
         for (int j = 0; j <= i; j++) {
             if (j == 0 || j == i) 
-                pascal[i][j] = 1;  // প্রথম ও শেষ মান সবসময় 1
+                pascal[i][j] = 1;
             else 
-                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];  // আগের row-এর উপর ভিত্তি করে মান বসানো
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]; 
             
-            cout << pascal[i][j] << " ";  // মান প্রিন্ট করা
+            cout << pascal[i][j] << " "; 
         }
         cout << endl;
     }
@@ -23,7 +23,7 @@ int main() {
     cout << "Enter number of rows: ";
     cin >> numRows;
 
-    generatePascalsTriangle(numRows);
+    pascalsTriangle(numRows);
 
     return 0;
 }
